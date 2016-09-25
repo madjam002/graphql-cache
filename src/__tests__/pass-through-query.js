cbb650c4e420198379dac38e43f96d72a649a52b
Jamie Greeff <jamie@greeff.me>
2016-09-25 15:23:12 +0100

    Get passThroughQuery working

--- src/__tests__/pass-through-query.js
+++ src/__tests__/pass-through-query.js
@@ -51,22 +51,10 @@ describe('passThroughQuery', function () {
 
     it('should take a complex query and result and return it in a cached format', function () {
       const query = gql`
-        query {
-          user {
+        fragment Bar on User {
+          andAnotherConnection {
             id
-            theUserName: name
-            myOtherName: name
-            about
-            myFriends: friends { id, name }
-            sameFriends: friends { id, name }
-            otherFriends: friends(limit: 10) { id, name }
-            otherFriendsDynamic: friends(limit: $someLimit) { id, name }
-            relatedFriends { id, name }
-            ...Foo
-            ...on User {
-              interests
-            }
-            dateOfBirth
+            name
           }
         }
 
@@ -78,10 +66,29 @@ describe('passThroughQuery', function () {
           ...Baz
         }
 
-        fragment Bar on User {
-          andAnotherConnection {
+        query {
+          user {
             id
-            name
+            theUserName: name
+            myOtherName: name
+            about
+            myFriends: friends { id, name }
+            sameFriends: friends { id, name }
+            otherFriends: friends(limit: 10) { id, name }
+            otherFriendsDynamic: friends(limit: $someLimit) { id, name }
+            moreFriendsDynamic: friends(limit: $someOtherLimit) { id, name }
+            relatedFriends {
+              id
+              name
+              friends(limit: $justOne) { name }
+              ...Another
+              ...AndAnother
+            }
+            ...Foo
+            ...on User {
+              interests
+            }
+            dateOfBirth
           }
         }
 
@@ -91,10 +98,21 @@ describe('passThroughQuery', function () {
             name
           }
         }
+
+        fragment Another on User {
+          about
+          interests
+        }
+
+        fragment AndAnother on User {
+          id
+        }
       `
 
       const variables = {
+        justOne: 1,
         someLimit: 2,
+        someOtherLimit: 3,
       }
 
       const cache = {
@@ -116,54 +134,75 @@ describe('passThroughQuery', function () {
             { id: '15', name: 'Person 5' },
             { id: '16', name: 'Person 6' },
           ],
-          relatedFriends: [
-            { id: '11', name: 'Person 1' },
-            { id: '12' },
-            { id: '13', name: 'Person 3' },
-          ],
-          someOtherConnection: [
+          [cacheKey('friends', { limit: 3 })]: [
             { id: '11', name: 'Person 1' },
             { id: '12', name: 'Person 2' },
+            { id: '13', name: 'Person 3' },
+          ],
+          relatedFriends: [
+            {
+              id: '11',
+              name: 'Person 1',
+              [cacheKey('friends', { limit: 1 })]: [
+                { id: '12', name: 'Person 2' },
+              ],
+              interests: 'Some',
+            },
+            {
+              id: '12',
+              // This friend doesn't have a name, so we need to fetch name field
+              [cacheKey('friends', { limit: 1 })]: [
+                { id: '11', name: 'Person 1' },
+              ],
+              interests: 'Some',
+            },
+            {
+              id: '13',
+              name: 'Person 3',
+              [cacheKey('friends', { limit: 1 })]: [
+                { id: '11', name: 'Person 1' },
+              ],
+              interests: 'Some',
+            },
+          ],
+          someOtherConnection: [
+            { name: 'Person 1' },
+            { name: 'Person 2' },
           ],
           reallyAnotherConnection: [
-            { id: '11', name: 'Person 1' },
+            { id: '20' },
           ],
         },
       }
 
-      const newQuery = print(passThroughQuery(cache, query))
+      const newQuery = print(passThroughQuery(cache, query, variables))
 
       expect(newQuery).to.equal(print(gql`
+        fragment Foo on User {
+          someOtherConnection {
+            id
+          }
+          ...Baz
+        }
+
         query {
           user {
             about
             otherFriendsDynamic: friends(limit: $someLimit) { id, name }
-            relatedFriends { name }
+            relatedFriends { name, ...Another }
             ...Foo
             dateOfBirth
           }
         }
 
-        fragment Foo on User {
-          someOtherConnection {
-            id
-            name
-          }
-          ...Baz
-        }
-
-        fragment Bar on User {
-          andAnotherConnection {
-            id
-            name
-          }
-        }
-
         fragment Baz on User {
           reallyAnotherConnection {
-            id
             name
           }
+        }
+
+        fragment Another on User {
+          about
         }
       `))
     })
--- src/pass-through-query.js
+++ src/pass-through-query.js
@@ -1,49 +1,93 @@
 import {visit} from 'graphql/language/visitor'
 
-const VISIT_SKIP_THIS_NODE = false
 const VISIT_REMOVE_NODE = null
 
-export function passThroughQuery(cache, query) {
-  return visitTree(query, [cache])
+export function passThroughQuery(cache, query, variables) {
+  const astPendingDeletion = visitTree(query, query, [cache], variables)
+
+  return visitTreeDeleteUnusedFragments(visitTreeDeleteNodes(astPendingDeletion))
 }
 
-function visitTree(ast, cacheStack) {
+function visitTree(rootAst, ast, cacheStack, variables, insideQuery = false) {
+  let skipAfter = null
+
   return visit(ast, {
 
-    enter(node, key, parent, path, ancestors) {
+    enter(node, key, parent, path) {
+      if (skipAfter) return false
+
+      if (node.kind === 'OperationDefinition' && node.operation === 'query') {
+        insideQuery = true
+        return
+      }
+
+      if (!insideQuery) {
+        return
+      }
+
+      if (node.kind === 'FragmentSpread') {
+        const nameOfFragment = node.name.value
+        const fragment = getFragment(rootAst, nameOfFragment)
+
+        const newFragment = {
+          ...fragment,
+          selectionSet: visitTree(rootAst, fragment.selectionSet, cacheStack, variables, true),
+        }
+
+        replaceFragment(rootAst, nameOfFragment, newFragment)
+
+        return false
+      }
+
       if (node.kind === 'Field') {
         const cacheStackTop = getTopOfStack(cacheStack)
 
         if (cacheStackTop == null) {
-          // console.log()
           return
         }
 
-        const cacheKey = getCacheKey(node)
+        const cacheKey = getCacheKey(node, variables)
         const selectionSet = node.selectionSet
 
         if (selectionSet) {
           if (Array.isArray(cacheStackTop[cacheKey])) {
-            // pushToStack(cacheStack, cacheStackTop[cacheKey])
-            //
-            // console.log('Visiting array', cacheKey)
-            // const res = visitArray(selectionSet, cacheStack)
-            //
-            // popTopFromStack(cacheStack)
-            //
-            // return res
+            pushToStack(cacheStack, cacheStackTop[cacheKey])
+
+            const res = visitArray(rootAst, node.selectionSet, cacheStack, variables)
+
+            const newNode = {
+              ...node,
+              selectionSet: res,
+            }
+
+            skipAfter = newNode
+
+            return newNode
           } else {
             pushToStack(cacheStack, cacheStackTop[cacheKey])
           }
         } else {
           if (cacheStackTop[cacheKey]) {
-            return VISIT_REMOVE_NODE
+            return markAsShouldDelete(node)
+          } else {
+            return markAsKeep(node)
           }
         }
       }
     },
 
     leave(node) {
+      if (skipAfter === node) skipAfter = null
+
+      if (node.kind === 'OperationDefinition' && node.operation === 'query') {
+        insideQuery = false
+        return
+      }
+
+      if (!insideQuery) {
+        return
+      }
+
       if (node.kind === 'Field') {
         const selectionSet = node.selectionSet
 
@@ -51,18 +95,100 @@ function visitTree(ast, cacheStack) {
           popTopFromStack(cacheStack)
         }
       }
-
-      // remove empty fragments
-      if (node.selectionSet && node.selectionSet.selections.length === 0) {
-        return VISIT_REMOVE_NODE
-      }
     },
 
   })
 }
 
-function getNewStackFrom(obj) {
-  return [obj]
+function visitTreeDeleteNodes(ast) {
+  return visit(ast, {
+    enter(node) {
+      if (node.__shouldDelete && node.kind === 'Field') {
+        return VISIT_REMOVE_NODE
+      }
+    },
+    leave(node) {
+      // remove empty fragments
+      if (node.selectionSet && node.selectionSet.selections.length === 0) {
+        return VISIT_REMOVE_NODE
+      }
+
+      if (node.kind === 'InlineFragment' && node.selectionSet === null) {
+        return VISIT_REMOVE_NODE
+      }
+    },
+  })
+}
+
+function visitTreeDeleteUnusedFragments(ast) {
+  const usedFragments = []
+
+  const newAst = visit(ast, {
+    enter(node) {
+      // remove fragmentspread's referencing non-existant fragments
+      if (node.kind === 'FragmentSpread') {
+        const nameOfFragment = node.name.value
+        const fragment = getFragment(ast, nameOfFragment)
+
+        if (!fragment) {
+          return VISIT_REMOVE_NODE
+        }
+
+        usedFragments.push(nameOfFragment)
+      }
+    },
+  })
+
+  return visit(newAst, {
+    enter(node) {
+      if (node.kind === 'FragmentDefinition') {
+        const nameOfFragment = node.name.value
+
+        if (!usedFragments.includes(nameOfFragment)) {
+          return VISIT_REMOVE_NODE
+        }
+      }
+    },
+  })
+}
+
+function markAsShouldDelete(node) {
+  if (node.__shouldDelete !== undefined) return
+
+  return {
+    ...node,
+    __shouldDelete: true,
+  }
+}
+
+function markAsKeep(node) {
+  return {
+    ...node,
+    __shouldDelete: false,
+  }
+}
+
+function getFragment(ast, name) {
+  if (ast.kind !== 'Document') {
+    throw new Error('getFragment(): ast.kind is not Document')
+  }
+
+  const { definitions } = ast
+
+  return definitions.find(def => def.name && def.name.value === name)
+}
+
+function replaceFragment(ast, name, newFragment) {
+  if (ast.kind !== 'Document') {
+    throw new Error('replaceFragment(): ast.kind is not Document')
+  }
+
+  const { definitions } = ast
+
+  const found = definitions.find(def => def.name && def.name.value === name)
+
+  Object.assign(found, newFragment)
+  return
 }
 
 function getTopOfStack(stack) {
@@ -77,7 +203,7 @@ function popTopFromStack(stack) {
   return stack.pop()
 }
 
-function getCacheKey(node) {
+function getCacheKey(node, variables) {
   const baseName = node.name.value
 
   if (node.arguments.length === 0) {
@@ -87,20 +213,32 @@ function getCacheKey(node) {
   const args = {}
 
   node.arguments.forEach(argument => {
-    args[argument.name.value] = argument.value.value
+    if (argument.value.kind === 'Variable') {
+      const variableName = argument.value.name.value
+
+      if (variables[variableName] == null) {
+        throw new Error(`getCacheKey(): Variable referenced "${variableName}" but not provided`)
+      }
+
+      return args[argument.name.value] = variables[variableName].toString()
+    } else {
+      args[argument.name.value] = argument.value.value
+    }
   })
 
   return baseName + '|' + JSON.stringify(args)
 }
 
-function visitArray(ast, cacheStack) {
+function visitArray(rootAst, ast, cacheStack, variables) {
   const cacheStackTop = getTopOfStack(cacheStack)
 
-  cacheStackTop.forEach((element, index) => {
-    cacheStackTop[index] = {}
+  let lastAst = ast
 
-    pushToStack(cacheStack, cacheStackTop[index])
-    visitTree(ast, cacheStack, getNewStackFrom(element))
+  cacheStackTop.forEach(element => {
+    pushToStack(cacheStack, element)
+    lastAst = visitTree(rootAst, lastAst, cacheStack, variables, true)
     popTopFromStack(cacheStack)
   })
+
+  return lastAst
 }

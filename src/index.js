0c2a0333ca3b241c96ffa889be06a08b195440a8
Jamie Greeff <jamie@greeff.me>
2016-09-28 19:55:50 +0100

    Expore sessionValidationMiddleware

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export {passThroughQuery} from './pass-through-query'
 export {queryCache} from './query-cache'
 
 export {normalizeEntities as normalizeEntitiesMiddleware} from './middleware/normalize-entities'
+export {sessionValidation as sessionValidationMiddleware} from './middleware/session-validation'
